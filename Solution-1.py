b698f477cc6e4a478a04b5486096d69f476fb0b3
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2019-03-16 23:45:27 +0000

    Code added

--- Solution-1.py
+++ Solution-1.py
@@ -4,4 +4,12 @@
 
 # enter the line is Cmder 'python solution-1.py'
 
+i = int(input("Enter a positive integer: ")) # i is any positive integer
 
+total = 1 # running total while I am doing the calculation - set to 0 initially
+
+while i > 1 :   # i is positive integer larger than 1
+    total = total + i #take total on the right, add the current value of i to it, and override the new value of total on the left
+    i = i - 1 # I want next time to come back with the smaller value of i
+
+print(total)
