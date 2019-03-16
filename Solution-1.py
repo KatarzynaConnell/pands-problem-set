8591f5f55058275856c6417dbb4a892cd1208852
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2019-03-16 23:31:03 +0000

    Added comments about the program and the path to enter in Cmder

--- Solution-1.py
+++ Solution-1.py
@@ -1,1 +1,7 @@
+# Kasia Connell, 2019
 # Solution to question 1
+# The program will ask user to enter any positive integer and it will output the sum of all the numbers between one and that number
+
+# enter the line is Cmder 'python solution-1.py'
+
+
