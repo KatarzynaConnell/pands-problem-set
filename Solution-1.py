d63cd109a932547cf4d630bcb0c30f92f6217683
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2019-03-17 20:28:31 +0000

    updates to comments

--- Solution-1.py
+++ Solution-1.py
@@ -6,10 +6,10 @@
 
 i = int(input("Enter a positive integer: ")) # i is any positive integer
 
-total = 1 # running total while I am doing the calculation - set to 0 initially
+total = 1 # running total while I am doing the calculation
 
-while i > 1 :   # i is positive integer larger than 1
-    total = total + i #take total on the right, add the current value of i to it, and override the new value of total on the left
-    i = i - 1 # I want next time to come back with the smaller value of i
+while i > 1 :   # i is positive integer larger than 1 - checking statement
+    total = total + i # take total on the right, add the current value of i to it, and override the new value of total on the left
+    i = i - 1 # I want next time to come back with the smaller value of i and get closer to the condition that i > 1 is false
 
-print(total)
+print(total) # show the result on the screen
