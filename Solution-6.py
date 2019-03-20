6735a23d76273c713f4141bee4fecb8e33625e2a
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2019-03-20 23:00:20 +0000

    Solution 6 started

---
+++ Solution-6.py
@@ -0,0 +1,8 @@
+# Kasia Connell, 2019
+# Solution to question 6
+# The program will take user's input string and output every second word
+
+# References: 
+# Ian's video tutorials: 
+# Ian's video tutorials: 
+
