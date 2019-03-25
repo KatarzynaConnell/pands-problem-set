2f15eece09e9726b03f568583be3d5dba44314aa
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2019-03-25 01:00:51 +0000

    changed problemset 6 function and prepared problem 7

--- Solution-6.py
+++ Solution-6.py
@@ -4,12 +4,13 @@
 
 # References: 
 # Ian's video tutorials: https://web.microsoftstream.com/video/909896e3-9d9d-45fe-aba0-a1930fe08a7f
-# Stackoverflow:https://stackoverflow.com/a/54857192 
+# Programiz: https://www.programiz.com/python-programming/methods/string/join
 # Python Documentation: https://docs.python.org/3/library/string.html
 # 
 # # enter the command in Cmder 'python solution-6.py'
 
 # User is asked to enter a string of text
-s = str(input("Enter a string of text:"))
-t = s.split(' ')[::2]           # split function to split string into words and next get every second word using []::2]
-print(t)                        # print the output
+string = (input("Enter a string of text:"))
+p = string.split()[::2]             # using string split function to separate string into words and get every second word by using [::2]
+separator = ' '                     # split function results in words with commas so separator 
+print(separator.join(p))
