839f01ce0c2a6f615f78ce02c7a2999de50eae5e
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2019-03-28 23:58:57 +0000

    Updated comments for Solution 8

--- Solution-8.py
+++ Solution-8.py
@@ -5,11 +5,12 @@
 # Ian's video tutorial https://web.microsoftstream.com/video/b8925ffa-0cf5-4fff-bfb0-b4664f704879
 # https://docs.python.org/3/library/datetime.html
 
-import datetime as dt 
-dt.datetime.now()
+# enter the command in Cmder 'python solution-8.py'
+import datetime as dt   # import datetime module to be able to use all the functionality in the module and call it dt 
+dt.datetime.now()       # the function datetime.now is entered to source the current time and date
 
 # format date into string using date format specifier
 # Adapted from https://docs.python.org/3/library/datetime.html
-today = dt.datetime.strftime(dt.datetime.now(), "%A, %B %d %Y at %H.%M%p")
+today = dt.datetime.strftime(dt.datetime.now(), "%A, %B %d %Y at %H.%M%p") # function that allows formatting the date and time in a specific way, 
 
-print(today)
+print(today)        # print the result
