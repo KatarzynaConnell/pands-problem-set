fa27d47a77b0ab6368a39ffa0ed47fcf49b2a351
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2019-03-30 09:58:28 +0000

    Code further udated'

--- Solution-9.py
+++ Solution-9.py
@@ -7,14 +7,14 @@
 # Ian's video about sys https://web.microsoftstream.com/video/65df155a-ac29-460b-869d-2de6ffc6c3fc
 # Import sys to execute the filename from the command line
 import sys
-# to get the single file name on the command line
-if len(sys.argv) == 1: # I just want the single file name on the command line in order to open the textfile, i.e. Solution-9 is the argument
-    print ("Opening textfile now") # Print confirmation the textfile is opening as if condition was met
-    with open("Sample.txt", "r") as f:
+# for the program to take the filename from the command line I set the condition to == 1
+if len(sys.argv) == 1: # the number of object to be entered on the command line in order to open the textfile is set to one, i.e. Solution-9 
+    print ("Opening Sample.txt now") # Print confirmation the textfile is opening
+    with open("Sample.txt", "r") as f:  # Open the text file 
         # To count the lines adopted the code from https://stackoverflow.com/a/30551984
-            for count, line in enumerate(f, start=0):
-                if count % 2 == 0:
-                    print(line)
+            for count, line in enumerate(f, start=0):  
+                if count % 2 == 0:      # if the line is even   
+                    print(line)         # print the line
 
 else:
-    print("You should enter a single filename")
+    print("You should enter a single filename") # if other combnation objects on the command line the text will not open and the output will advise to enter single file name
