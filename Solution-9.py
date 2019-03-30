39038823ca990b28fc9cf9255e6e66fced524f71
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2019-03-30 09:17:50 +0000

    added sample text file and unpdated the program

--- Solution-9.py
+++ Solution-9.py
@@ -3,4 +3,18 @@
 # The program will read in a text file and output every second line. The program should take the filename from an argument on the command line
 
 # Refernces: 
-# Ian's video tutorial https://web.microsoftstream.com/video/b8925ffa-0cf5-4fff-bfb0-b4664f704879
+# Ian's video tutorial about modules https://web.microsoftstream.com/video/b8925ffa-0cf5-4fff-bfb0-b4664f704879
+# Ian's video about sys https://web.microsoftstream.com/video/65df155a-ac29-460b-869d-2de6ffc6c3fc
+# Import sys to execute the filename from the command line
+import sys
+# to get the single file name on the command line
+if len(sys.argv) == 1: # I just want the single file name on the command line in order to open the textfile, i.e. Solution-9 is the argument
+    print ("Opening textfile now") # Print confirmation the textfile is opening as if condition was met
+    with open("Sample.txt", "r") as f:
+        # To count the lines adopted the code from https://stackoverflow.com/a/30551984
+            for count, line in enumerate(f, start=0):
+                if count % 2 == 0:
+                    print(line)
+
+else:
+    print("You should enter a single filename")
