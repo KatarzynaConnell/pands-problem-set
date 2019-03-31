5275daeb766ddbe8022e0d5c9142e854ecc45212
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2019-03-31 18:07:18 +0100

    Solution-10 updated

--- Solution-10.py
+++ Solution-10.py
@@ -2,28 +2,40 @@
 # Solution to Question 10
 # The Program will output the plot of functions x, power x and 2 to the power of x in the range [0,4]
 
-#Imort math solution 
-import math
 #Import numpy solution for the purpose of plot
-import numpy
+import numpy as np
+#Import matplotlib to show data on the plot
+import matplotlib.pyplot as pl
 # set the range of x from 0 to 4
-x = range(0,5)
+x = np.arange(0,5)
 
-# to calculate the power of x:
-for x in range(0,5): 
-    f1 = x**2
-#to calculate 2 to the power of x:
-    f2= 2**x
-    print(x, f1, f2)  # Print all the value of x, x to the powoer of 2, 2 to the power of x
+# list of the 3 functions required:
+f1 = x
+f2 = x**2
+f3 = 2**x
     
-# import matplot pyplot and assign it name pl
-import matplotlib.pyplot as pl  
-# for the plot to be created enter:
-pl.hist(x)  # command to create the plot for value x
-pl.show(x)  # command to show the plot for value x
- 
-pl.hist(f1) # command to create the plot for f1
-pl.show(f1) # command to show the plot for f1
+# Set propertie of the line functions:    
+# Adopted from https://matplotlib.org/users/pyplot_tutorial.html 
+pl.plot(x, f1, 'r') # red line
 
-pl.hist(f2) # command to create the plot for f2
-pl.show(f2) # command to show the plot for f2
+pl.plot(x, f2, 'y') # yellow line
+
+pl.plot(x, f3, 'k') # black line
+
+# Graph names 
+pl.xlabel('X')
+pl.ylabel('Y')
+pl.title('x, power x and 2 to the power of x') 
+
+#Legend details
+
+pl.legend(['f1 = x', 'f2 = x^2', 'f3 = 2^x'])
+
+
+#Show grid lines
+
+pl.grid(axis='both')
+
+#display graph
+
+pl.show()
