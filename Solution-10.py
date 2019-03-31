5caefe90c103a6378fadf92844bec303b30f00c2
Katarzyna Connell <katarzyna.connell@yahoo.ie>
2019-03-31 15:26:52 +0100

    finished Solution-10 and updated comments

--- Solution-10.py
+++ Solution-10.py
@@ -2,23 +2,28 @@
 # Solution to Question 10
 # The Program will output the plot of functions x, power x and 2 to the power of x in the range [0,4]
 
+#Imort math solution 
 import math
+#Import numpy solution for the purpose of plot
 import numpy
+# set the range of x from 0 to 4
+x = range(0,5)
 
-x = int
+# to calculate the power of x:
+for x in range(0,5): 
+    f1 = x**2
+#to calculate 2 to the power of x:
+    f2= 2**x
+    print(x, f1, f2)  # Print all the value of x, x to the powoer of 2, 2 to the power of x
+    
+# import matplot pyplot and assign it name pl
+import matplotlib.pyplot as pl  
+# for the plot to be created enter:
+pl.hist(x)  # command to create the plot for value x
+pl.show(x)  # command to show the plot for value x
+ 
+pl.hist(f1) # command to create the plot for f1
+pl.show(f1) # command to show the plot for f1
 
-for x in range(0,5):
-    power1 = x**2
-    power2 = 2**x
-    print(x, power1, power2)
-    continue
-import matplotlib.pyplot as pl 
-pl.plot([x])
-pl.ylabel(power1)
-pl.hist(x)
-pl.show()
-
-#pl.hist(power1)
-#pl.show(power1)
-#pl.hist(power2)
-#pl.show(power2)
+pl.hist(f2) # command to create the plot for f2
+pl.show(f2) # command to show the plot for f2
